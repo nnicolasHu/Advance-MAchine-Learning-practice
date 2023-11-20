d9d7d1cbcbdede3e5b795ba303a99971412f7a7b
Guillaume SALLE <guillaume.salle@gmail.com>
2023-11-20 23:58:06 +0100

    copy RNN TP5

--- student_tp5/src/tp5.py
+++ student_tp5/src/tp5.py
@@ -27,6 +27,53 @@ def maskedCrossEntropy(output: torch.Tensor, target: torch.LongTensor, padcar: i
 
 class RNN(nn.Module):
     #  TODO:  Recopier l'implémentation du RNN (TP 4)
+    def __init__(self, dimx, dim_latent, dim_output):
+        super().__init__()
+        self.dimx = dimx
+        self.dim_latent = dim_latent
+        self.dim_output = dim_output
+
+        self.l1 = nn.Linear(dimx, dim_latent, bias=False)
+        self.l2 = nn.Linear(dim_latent, dim_latent)
+        self.activation1 = nn.Tanh()
+
+        # Le decodeur est independant de la classe et peut etre implemente en dehors
+        self.l_decode = nn.Linear(dim_latent, dim_output)
+
+
+    def one_step(self, x, h):
+        """
+        input : - x_t de dimention (batch x dimx)
+                - h de dimension (batch x latent)
+        output :- matrice de tous les etats caches (batch x latent)
+        """
+        return self.activation1(self.l1(x) + self.l2(h))
+    
+
+    def forward(self, x, h_0=None):
+        """
+        input : - x de dimention (batch x length x dimx)
+                - h_0 de dimension (batch x latent)
+        output :- matrice de tous les etats caches (lenght x batch x latent)
+        """
+        if h_0 is None:
+            h_0 = torch.zeros(x.shape[0], self.dim_latent)
+        res = [h_0]
+        
+
+        for i in range(1, x.size(1)):
+            res.append(self.one_step(x[:,i-1,:], res[i-1]))
+        
+        return torch.stack(res, dim=0)
+
+
+    def decode(self, h):
+        """
+        input : - h de dimention (batch x latent)
+        output :- matrice des vecteurs one-hot sortie de taille (batch x dim_output)
+        """
+        return self.l_decode(h)
+
 
 
 class LSTM(RNN):
